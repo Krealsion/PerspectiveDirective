d354fcd8c9b4ed889cf06f8b4393a91652ba42ce
KnightRaven0 <firstwinter1@gmail.com>
2018-04-13 10:05:33 -0600

    Fixed Lookup Tables
    Corrected the generaton of lookup tables to be odd, as well as rounded the lookup to -PI - PI

--- jMath.java
+++ jMath.java
@@ -2,31 +2,47 @@ package pkg3dengine;
 
 public class jMath {
 
-	public static final double PI = 3.141592653589793238462643383279502884197;
+	public static final double PI   = 3.141592653589793238462643383279502884197;
 	public static final double PIo2 = 1.570796326794896619231321691639751442098;
+	public static final double PIo4 = 0.785398163397448309615660845819875721049;
 	public static final double PIt2 = 6.283185307179586476925286766559005768394;
 	public static double[] SineLookup;
 	public static double[] CosineLookup;
 
 	static {
-		GenerateTrigLookupTables(100000);
+		GenerateTrigLookupTables(10001);
 	}
 
 	public static void GenerateTrigLookupTables(int Size) {
-		SineLookup = new double[Size];
-		CosineLookup = new double[Size];
-		for (int i = 0; i < Size; i++) {
-			SineLookup[i] = Math.sin(PI * (((double) i / ((double) Size / 2d)) - 1d));
+		if (Size % 2 == 0){		//SIZE SHOULD ALWAYS BE ODD TO MAINTAIN SYMETRY
+			Size++;
+		}
+		SineLookup = new double[Size];		//Initialize Arrays
+		CosineLookup = new double[Size];	//Initialize Arrays
+		for (int i = 0; i < Size; i++) {	//For each value
+			SineLookup[i] = Math.sin(PI * (((double) i / ((double) Size / 2d)) - 1d));		//Get it's relative percentage through the array, divide by two, then
 			CosineLookup[i] = Math.cos(PI * (((double) i / ((double) Size / 2d)) - 1d));
 		}
 	}
 
 	public static double FastSin(double Angle) {
-		return SineLookup[(int) (((Angle / PI) + 1d) * (double) SineLookup.length / 2 - 1)];
+		if (Angle > PI) {
+			Angle -= PIt2;
+		}
+		if (Angle < -PI) {
+			Angle += PIt2;
+		}
+		return SineLookup[(int) ((Angle / PI) * (double) ((SineLookup.length - 1) / 2) + (SineLookup.length - 1) / 2)];
 	}
-
+	
 	public static double FastCos(double Angle) {
-		return CosineLookup[(int) (((Angle / PI) + 1d) * (double) CosineLookup.length / 2 - 1)];
+		if (Angle > PI) {
+			Angle -= PIt2;
+		}
+		if (Angle < -PI) {
+			Angle += PIt2;
+		}
+		return CosineLookup[(int) ((Angle / PI) * (double) ((CosineLookup.length - 1) / 2) + (CosineLookup.length - 1) / 2)];
 	}
 
 	public static double getRadiansPointsTo(Vector2 From, Vector2 To) {
